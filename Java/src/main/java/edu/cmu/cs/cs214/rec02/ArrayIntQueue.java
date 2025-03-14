71547a0d9b5577aa11292fdb971064a52aa010ed
Nursultan <nursultandaurenbek6@gmail.com>
2025-03-14 13:34:16 +0800

    Shineshlel

--- Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
@@ -1,57 +1,36 @@
 package edu.cmu.cs.cs214.rec02;
-
+ 
 import java.util.Arrays;
-
+ 
 /**
  * A resizable-array implementation of the {@link IntQueue} interface. The head of
  * the queue starts out at the head of the array, allowing the queue to grow and
  * shrink in constant time.
  *
- * TODO: This implementation contains three bugs! Use your tests to determine the
- * source of the bugs and correct them!
- *
  * @author Alex Lockwood
  * @author Ye Lu
  */
 public class ArrayIntQueue implements IntQueue {
-
-    /**
-     * An array holding this queue's data
-     */
+ 
     private int[] elementData;
-
-    /**
-     * Index of the next dequeue-able value
-     */
     private int head;
-
-    /**
-     * Current size of queue
-     */
     private int size;
-
-    /**
-     * The initial size for new instances of ArrayQueue
-     */
     private static final int INITIAL_SIZE = 10;
-
-    /**
-     * Constructs an empty queue with an initial capacity of ten.
-     */
+ 
     public ArrayIntQueue() {
         elementData = new int[INITIAL_SIZE];
         head = 0;
         size = 0;
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public void clear() {
         Arrays.fill(elementData, 0);
         size = 0;
         head = 0;
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public Integer dequeue() {
         if (isEmpty()) {
             return null;
@@ -61,8 +40,8 @@ public class ArrayIntQueue implements IntQueue {
         size--;
         return value;
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public boolean enqueue(Integer value) {
         ensureCapacity();
         int tail = (head + size) % elementData.length;
@@ -70,36 +49,32 @@ public class ArrayIntQueue implements IntQueue {
         size++;
         return true;
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public boolean isEmpty() {
-        return size >= 0;
+        return size == 0;
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public Integer peek() {
+        if (isEmpty()) {
+            return null;
+        }
         return elementData[head];
     }
-
-    /** {@inheritDoc} */
+ 
+    @Override
     public int size() {
         return size;
     }
-
-    /**
-     * Increases the capacity of this <tt>ArrayIntQueue</tt> instance, if
-     * necessary, to ensure that it can hold at least size + 1 elements.
-     */
+ 
     private void ensureCapacity() {
         if (size == elementData.length) {
             int oldCapacity = elementData.length;
             int newCapacity = 2 * oldCapacity + 1;
             int[] newData = new int[newCapacity];
-            for (int i = head; i < oldCapacity; i++) {
-                newData[i - head] = elementData[i];
-            }
-            for (int i = 0; i < head; i++) {
-                newData[head - i] = elementData[i];
+            for (int i = 0; i < size; i++) {
+                newData[i] = elementData[(head + i) % oldCapacity];
             }
             elementData = newData;
             head = 0;
--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -1,25 +1,27 @@
 package edu.cmu.cs.cs214.rec02;
-
-import org.junit.Before;
-import org.junit.Test;
-
+ 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import static org.junit.Assert.*;
-
-
+ 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+ 
+ 
 /**
- * TODO: 
+ * TODO:
  * 1. The {@link LinkedIntQueue} has no bugs. We've provided you with some example test cases.
- * Write your own unit tests to test against IntQueue interface with specification testing method 
+ * Write your own unit tests to test against IntQueue interface with specification testing method
  * using mQueue = new LinkedIntQueue();
- * 
- * 2. 
+ *
+ * 2.
  * Comment `mQueue = new LinkedIntQueue();` and uncomment `mQueue = new ArrayIntQueue();`
  * Use your test cases from part 1 to test ArrayIntQueue and find bugs in the {@link ArrayIntQueue} class
  * Write more unit tests to test the implementation of ArrayIntQueue, with structural testing method
@@ -28,69 +30,87 @@ import static org.junit.Assert.*;
  * @author Alex Lockwood, George Guo, Terry Li
  */
 public class IntQueueTest {
-
+ 
     private IntQueue mQueue;
     private List<Integer> testList;
-
+ 
     /**
      * Called before each test.
      */
     @Before
     public void setUp() {
-        // comment/uncomment these lines to test each class
-        mQueue = new LinkedIntQueue();
-    //    mQueue = new ArrayIntQueue();
-
+        // Use the ArrayIntQueue for testing
+        mQueue = new ArrayIntQueue();  // Use for ArrayIntQueue
+        // mQueue = new LinkedIntQueue();  // Uncomment to test LinkedIntQueue
         testList = new ArrayList<>(List.of(1, 2, 3));
     }
-
+ 
     @Test
     public void testIsEmpty() {
-        // This is an example unit test
         assertTrue(mQueue.isEmpty());
     }
-
+ 
     @Test
     public void testNotEmpty() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(5);
+        assertFalse(mQueue.isEmpty());
     }
-
+ 
     @Test
     public void testPeekEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        assertNull(mQueue.peek());  // Queue is empty, so peek should return null
     }
-
+ 
     @Test
     public void testPeekNoEmptyQueue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(10);
+        assertEquals(Integer.valueOf(10), mQueue.peek());  // Should return the first enqueued element
     }
-
+ 
     @Test
     public void testEnqueue() {
-        // This is an example unit test
         for (int i = 0; i < testList.size(); i++) {
             mQueue.enqueue(testList.get(i));
             assertEquals(testList.get(0), mQueue.peek());
             assertEquals(i + 1, mQueue.size());
         }
     }
-
+ 
     @Test
     public void testDequeue() {
-        // TODO: write your own unit test
-        fail("Test not implemented");
+        mQueue.enqueue(1);
+        mQueue.enqueue(2);
+        assertEquals(Integer.valueOf(1), mQueue.dequeue());
+        assertEquals(Integer.valueOf(2), mQueue.dequeue());
+        assertNull(mQueue.dequeue());  // After dequeueing all elements, it should return null
     }
-
+ 
+    @Test
+    public void testEnsureCapacity() {
+        // Test for the case when the queue size exceeds the initial capacity
+        for (int i = 0; i < 15; i++) {
+            mQueue.enqueue(i);
+        }
+        assertEquals(15, mQueue.size());  // Expect the size to be 15 after adding more than initial capacity
+    }
+ 
+    @Test
+    public void testClear() {
+        mQueue.enqueue(1);
+        mQueue.enqueue(2);
+        mQueue.clear();
+        assertTrue(mQueue.isEmpty());  // After clear, the queue should be empty
+        assertNull(mQueue.peek());  // Peek should return null as queue is cleared
+    }
+ 
+ 
     @Test
     public void testContent() throws IOException {
         // This is an example unit test
         InputStream in = new FileInputStream("src/test/resources/data.txt");
         try (Scanner scanner = new Scanner(in)) {
             scanner.useDelimiter("\\s*fish\\s*");
-
+ 
             List<Integer> correctResult = new ArrayList<>();
             while (scanner.hasNextInt()) {
                 int input = scanner.nextInt();
@@ -98,12 +118,12 @@ public class IntQueueTest {
                 System.out.println("enqueue: " + input);
                 mQueue.enqueue(input);
             }
-
+ 
             for (Integer result : correctResult) {
                 assertEquals(mQueue.dequeue(), result);
             }
         }
     }
-
-
+ 
+ 
 }
